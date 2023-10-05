9647df51927e8bee4d9377f69cfa1c39c2eee5b2
AliciaGM <alicia.gutierrez87@gmail.com>
2023-10-05 10:06:59 +0200

    ejercicio selectores, creators and inserts

---
+++ 01-basics/ut-3-vanillajs-dom/01-selectors/main.js
@@ -0,0 +1,17 @@
+let title = document.getElementById("titulo");
+console.log(title.textContent);
+
+let paragraph = document.getElementsByClassName("parrafo");
+for (let i = 0; i < paragraph.length; i++) {
+    console.log(paragraph[i].textContent);
+  }
+
+let n = document.getElementsByName("nombre");
+console.log(n);
+
+let a = document.getElementsByName("apellido");
+console.log(a);
+
+
+  
+
---
+++ 01-basics/ut-3-vanillajs-dom/02-selectors/main.js
@@ -0,0 +1,19 @@
+let img = document.createElement("img");
+img.src = "img_la.jpg";
+document.body.append(img);
+img.alt = "LA";
+
+let h1 = document.createElement("h1");
+h1.textContent = "This is a Heading";
+document.body.append(h1);
+
+let p1 = document.createElement("p");
+p1.textContent = "This is a paragraph";
+document.body.append(p1);
+
+let p2 = document.createElement("p");
+p2.textContent = "This is a another paragraph";
+document.body.append(p2);
+
+
+
