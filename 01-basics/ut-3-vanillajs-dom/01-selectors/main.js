4664c84a51feccf4dcc9e6a728205c85335bc534
AliciaGM <alicia.gutierrez87@gmail.com>
2023-10-05 18:41:15 +0200

    termino el ejercicio, solo falta un apartado por hacer

--- 01-basics/ut-3-vanillajs-dom/01-selectors/main.js
+++ 01-basics/ut-3-vanillajs-dom/01-selectors/main.js
@@ -7,11 +7,24 @@ for (let i = 0; i < paragraph.length; i++) {
   }
 
 let n = document.getElementsByName("nombre");
-console.log(n);
+let primerElemNombre = n[0];
+let valorNombre = primerElemNombre.nodeValue;
 
 let a = document.getElementsByName("apellido");
-console.log(a);
 
+console.log(primerElemNombre);
 
+let elemento1 = document.getElementsByTagName("li");
+for (let i = 0; i < elemento1.length; i++) {
+  console.log(elemento1[i].textContent);
+}
+
+let tituloPag = document.querySelector("h1");
+console.log(tituloPag.textContent);
+
+let parrafos = document.querySelectorAll("p");
+for (let i = 0; i < parrafos.length; i++) {
+  console.log(parrafos[i].textContent);
+}
   
 
